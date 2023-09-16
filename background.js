632a3f3bd1dafbdff776488384c6d4bb88836fb4
thijsdc <35472839+thijsdc@users.noreply.github.com>
2023-09-16 15:59:59 +0200

    some simplifications

--- background.js
+++ background.js
@@ -1,31 +1,12 @@
 'use strict';
 
-chrome.alarms.onAlarm.addListener(() => {
-  chrome.action.setBadgeText({ text: '' });
+chrome.alarms.onAlarm.addListener(() => {  
   chrome.notifications.create({
     type: 'basic',
     iconUrl: 'spine16.png',
     title: 'Correct posture!',
-    message: "Correct your posture!",
-    // buttons: [{ title: 'Done!' }],
+    message: "Correct your posture!",    
     priority: 0
   });
 });
 
-// chrome.notifications.onButtonClicked.addListener(async () => {
-//   const storageData = await chrome.storage.sync.get(['min']);
-//   const minValue = storageData.min;
-//   console.log(`The min value is: ${minValue}`);
-//   const minutes = getRandomNumber(minValue, 60); 
-//   chrome.action.setBadgeText({ text: 'ON' });
-//   chrome.alarms.create({ delayInMinutes: minutes});
-//   console.log(`Next alarm is in ${minutes}`);
-// });
-
-
-// function getRandomNumber(min, max) {
-//   const randomDecimal = Math.random();  
-//   const randomInRange = randomDecimal * (max - min) + min;  
-//   const randomInteger = Math.floor(randomInRange);
-//   return randomInteger;
-// }
